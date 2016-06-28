f2e27854f2498b2c7459e7b0ece3570bff8d38d6
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-06-27 19:12:19 -0700

    Create Segue to transition out of Splash Screen

---
+++ LocNotes/SplashScreenController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LocNotes
+//
+//  Created by axe2 on 6/22/16.
+//  Copyright © 2016 axe. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Setup the timer to hide the splash screen and goto the next screen
+        _ = NSTimer.scheduledTimerWithTimeInterval(
+            2, target: self, selector: #selector(SplashScreenController.switchOutToListScreen), userInfo: nil, repeats: false
+        )
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func switchOutToListScreen() {
+        performSegueWithIdentifier("exitSplashScreen", sender: self)
+    }
+    
+}
+
