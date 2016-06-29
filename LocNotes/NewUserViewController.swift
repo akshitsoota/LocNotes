2a50a16120fc6fa6d325afb580082bb35fc6ddbc
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-06-29 11:54:36 -0700

    Add test code to get scroll color change to work

--- LocNotes/NewUserViewController.swift
+++ LocNotes/NewUserViewController.swift
@@ -11,17 +11,22 @@
 
 import UIKit
 
-class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
+class NewUserViewController: UIViewController, UIPageViewControllerDataSource, UIScrollViewDelegate {
     
     @IBOutlet weak var buttonsStackView: UIStackView!
     
     var pageViewController: UIPageViewController!
     var pageViews: [String] = []
+    var pageViewBackgrounds: [UIColor] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Initialze the pageView array with the identifiers for the PageViewController pages
         pageViews.append("NewUserPageView1")
+        pageViews.append("NewUserPageView1")
+        
+        pageViewBackgrounds.append(UIColor(red: 246/255, green: 169/255, blue: 26/255, alpha: 1))
+        pageViewBackgrounds.append(UIColor(red: 26/255, green: 169/255, blue: 246/255, alpha: 1))
         
         // Also initialize the PageViewController
         self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("NewUserPageView") as! UIPageViewController
@@ -34,6 +39,13 @@ class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
         
         self.pageViewController.setViewControllers(viewControllers, direction: .Forward, animated: true, completion: nil)
         
+        //
+        for view in self.pageViewController.view.subviews {
+            if let scrollView = view as? UIScrollView {
+                scrollView.delegate = self
+            }
+        }
+        
         // Now add this to our screen
         self.pageViewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height - self.buttonsStackView.frame.height - 20)
         self.addChildViewController(pageViewController)
@@ -41,7 +53,7 @@ class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
         self.pageViewController.didMoveToParentViewController(self)     // We will be handling all those function calls
         
         // Set a background for the first page
-        self.view.backgroundColor = startingPage.view.backgroundColor
+        self.view.backgroundColor = pageViewBackgrounds[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -99,6 +111,31 @@ class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
     func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
         // Returns the initial page index
         return 0
+    }
+    
+    // MARK: - UIScrollViewDelegate Methods
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        NSLog("scrollViewDidScrollCalled \(self.view.frame.width)")
+        //
+        let oldColor = self.pageViewBackgrounds[0]
+        let newColor = self.pageViewBackgrounds[1]
+        //
+        var oR: CGFloat = 0, oG: CGFloat = 0, oB: CGFloat = 0, oA: CGFloat = 0
+        var nR: CGFloat = 0, nG: CGFloat = 0, nB: CGFloat = 0, nA: CGFloat = 0
+        oldColor.getRed(&oR, green: &oG, blue: &oB, alpha: &oA)
+        newColor.getRed(&nR, green: &nG, blue: &nB, alpha: &nA)
+        // Resolve percentage of scroll
+        let pscroll = ((scrollView.contentOffset.x - self.view.frame.width) / self.view.frame.width)
+        // Solve new color
+        let pR = (nR - oR) * pscroll
+        let pG = (nG - oG) * pscroll
+        let pB = (nB - oB) * pscroll
+        // Color
+        self.view.backgroundColor = UIColor(red: pR + oR, green: pG + oG, blue: pB + oB, alpha: 1)
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        NSLog("scrollViewWillBeginDragging \(scrollView.contentOffset)")
     }
 
 }
