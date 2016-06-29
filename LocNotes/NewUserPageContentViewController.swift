cc78a8bd0f6ca34d6d98741885e57aee291f1f4a
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-06-29 09:54:51 -0700

    Basic one page new user setup done
    Need to reorder things to show up the buttons

---
+++ LocNotes/NewUserPageContentViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewUserPageContentViewController.swift
+//  LocNotes
+//
+//  Created by Akshit (Axe) Soota on 6/29/16.
+//  Copyright © 2016 axe. All rights reserved.
+//
+
+import UIKit
+
+class NewUserPageContentViewController: UIViewController {
+
+    var pageIndex: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
--- LocNotes/NewUserViewController.swift
+++ LocNotes/NewUserViewController.swift
@@ -6,16 +6,34 @@
 //  Copyright © 2016 axe. All rights reserved.
 //
 
+// Useful UIPageViewController Tutorial used:
+//   https://www.youtube.com/watch?v=8bltsDG2ENQ
+
 import UIKit
 
 class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
     
     var pageViewController: UIPageViewController!
-    var pageViews: [UIViewController] = []
+    var pageViews: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Initialze the pageView array with the identifiers for the PageViewController pages
+        pageViews.append("NewUserPageView1")
+        // Also initialize the PageViewController
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("NewUserPageView") as! UIPageViewController
+        self.pageViewController.dataSource = self   // This very class will be the UIPageViewControllerDataSource for the pages
+        // Setup the starting Page to be shown on the UIPageViewController
+        let startingPage = self.getViewControllerAtIndex(0) as! NewUserPageContentViewController
+        var viewControllers: [UIViewController] = []
+        viewControllers.append(startingPage)
         
+        self.pageViewController.setViewControllers(viewControllers, direction: .Forward, animated: true, completion: nil)
+        // Now add this to our screen
+        self.pageViewController.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        self.addChildViewController(pageViewController)
+        self.view.addSubview(pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)     // We will be handling all those function calls
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,8 +46,51 @@ class NewUserViewController: UIViewController, UIPageViewControllerDataSource {
         if( index >= self.pageViews.count ) {
             return UIViewController() // Out Of Bounds, so return a new UIViewController
         }
-        // Else, fetch an return
-        return self.pageViews[index]
+        // Else, instantiate and return
+        let viewController = self.storyboard?.instantiateViewControllerWithIdentifier(self.pageViews[index]) as! NewUserPageContentViewController
+        viewController.pageIndex = index
+        // Return
+        return viewController
+    }
+    
+    // MARK: - Page View Controller Data Source
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        // Presents the page before the current one
+        let viewController = viewController as! NewUserPageContentViewController
+        let pageIndex = viewController.pageIndex as Int
+        
+        // Check if left out of bounds
+        if( pageIndex == 0 || pageIndex == NSNotFound ) {
+            return nil          // We've nothing to show
+        }
+        // Else, resolve and send back
+        return getViewControllerAtIndex(pageIndex - 1)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        // Presents the page after the current one
+        let viewController = viewController as! NewUserPageContentViewController
+        let pageIndex = viewController.pageIndex as Int
+        
+        // Check if not found or right out of bounds
+        if( pageIndex == self.pageViews.count - 1 || pageIndex == NSNotFound ) {
+            return nil          // We've nothing to show
+        }
+        // Else, resolve and send back
+        return getViewControllerAtIndex(pageIndex + 1)
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        // Returns the total count of the number of pages
+        return self.pageViews.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        // Returns the initial page index
+        return 0
     }
 
 }
