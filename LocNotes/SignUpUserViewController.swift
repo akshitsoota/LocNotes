54c4fa965544863d4840a6720f4744332bafa62d
Akshit (Axe) Soota <akshitsoota@users.noreply.github.com>
2016-07-02 19:59:29 -0700

    Finish up Signup User GUI and partially finish up its view controller

--- LocNotes/SignUpUserViewController.swift
+++ LocNotes/SignUpUserViewController.swift
@@ -1,23 +1,25 @@
 //
-//  LoginUserViewController.swift
+//  SignUpUserViewController.swift
 //  LocNotes
 //
-//  Created by Akshit (Axe) Soota on 7/1/16.
+//  Created by Akshit (Axe) Soota on 7/2/16.
 //  Copyright © 2016 axe. All rights reserved.
 //
 
 import UIKit
 
-class LoginUserViewController: UIViewController, UIGestureRecognizerDelegate, UITextFieldDelegate {
-    
+class SignUpUserViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var signupButtonViewHolder: UIView!
-
+    @IBOutlet weak var loginButtonViewHolder: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Setup the username and password fields
-        setupUsernamePasswordFields()
+        // Setup the fields
+        setupFields()
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,9 +38,20 @@ class LoginUserViewController: UIViewController, UIGestureRecognizerDelegate, UI
         UIApplication.sharedApplication().statusBarStyle = .Default
     }
     
+    // MARK: - Force Screen Orientation
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
+    }
+    
     // MARK: - viewDidLoad() setup functions
-    func setupUsernamePasswordFields() {
+    func setupFields() {
         // Remove the default border off the username and password fields
+        self.nameField.borderStyle = .None
+        self.emailField.borderStyle = .None
         self.usernameField.borderStyle = .None
         self.passwordField.borderStyle = .None
         
@@ -55,6 +68,18 @@ class LoginUserViewController: UIViewController, UIGestureRecognizerDelegate, UI
         passwordBorder.frame = CGRect(x: 0, y: self.passwordField.frame.size.height - borderWidth, width: self.passwordField.frame.size.width, height: self.passwordField.frame.size.height)
         passwordBorder.borderWidth = borderWidth
         
+        // Similarly, set one up for the name field
+        let nameBorder = CALayer()
+        nameBorder.borderColor = UIColor.whiteColor().CGColor
+        nameBorder.frame = CGRect(x: 0, y: self.nameField.frame.size.height - borderWidth, width: self.nameField.frame.size.width, height: self.nameField.frame.size.height)
+        nameBorder.borderWidth = borderWidth
+        
+        // Similarly, set one up for the email field
+        let emailBorder = CALayer()
+        emailBorder.borderColor = UIColor.whiteColor().CGColor
+        emailBorder.frame = CGRect(x: 0, y: self.emailField.frame.size.height - borderWidth, width: self.emailField.frame.size.width, height: self.emailField.frame.size.height)
+        emailBorder.borderWidth = borderWidth
+        
         // Set it to both the fields now
         self.usernameField.layer.addSublayer(usernameBorder)
         self.usernameField.layer.masksToBounds = true
@@ -62,80 +87,62 @@ class LoginUserViewController: UIViewController, UIGestureRecognizerDelegate, UI
         self.passwordField.layer.addSublayer(passwordBorder)
         self.passwordField.layer.masksToBounds = true
         
+        self.nameField.layer.addSublayer(nameBorder)
+        self.nameField.layer.masksToBounds = true
+        
+        self.emailField.layer.addSublayer(emailBorder)
+        self.emailField.layer.masksToBounds = true
+        
         // Set password field to a secure entry field
         self.passwordField.secureTextEntry = true
         
         // Have all the return events come to us
         self.usernameField.delegate = self
         self.passwordField.delegate = self
+        self.nameField.delegate = self
+        self.emailField.delegate = self
     }
     
     // MARK: - Actions received here
-    @IBAction func loginButtonClicked(sender: AnyObject) {
+    @IBAction func signupButtonClicked(sender: AnyObject) {
         // Cast the sender to a UIButton
         let senderButton: UIButton! = sender as! UIButton
-        // Set the login button background back to the default
+        // Set the signup button background back to the default
         senderButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
-        // Start the login process
-        attemptLogin()
+        // Start the signup process
+        attemptSignup()
     }
     
-    @IBAction func loginButtonPressedDown(sender: AnyObject) {
+    @IBAction func signupButtonPressedDown(sender: AnyObject) {
         let senderButton: UIButton! = sender as! UIButton
         // Now let us deal with the background of the sender button
         senderButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
     }
     
-    @IBAction func loginButtonTouchCancelled(sender: AnyObject) {
+    @IBAction func signupButtonTouchCancelled(sender: AnyObject) {
         // Cast the sender to a UIButton
         let senderButton: UIButton! = sender as! UIButton
-        // Set the login button background back to the default
+        // Set the signup button background back to the default
         senderButton.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
     }
     
-    @IBAction func signupButtonClicked(sender: AnyObject) {
-        // Reset the background of the parent view
-        signupButtonViewHolder.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
-        // Now perform the segue
-        performSegueWithIdentifier("showSignupUser", sender: self)
-    }
-    
-    @IBAction func signupButtonPressedDown(sender: AnyObject) {
-        // Deal with the background of the parent view
-        signupButtonViewHolder.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
-    }
-    
-    @IBAction func signupButtonTouchCancelled(sender: AnyObject) {
-        // Reset the background of the parent view
-        signupButtonViewHolder.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.4)
-    }
-    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        if( textField == self.usernameField ) {
+        if( textField == self.nameField ) {
+            self.emailField.becomeFirstResponder() // Let the email field take over
+        } else if( textField == self.emailField ) {
+            self.usernameField.becomeFirstResponder() // Let the username field take over
+        } else if( textField == self.usernameField ) {
             self.passwordField.becomeFirstResponder() // Let the password field take over
         } else if( textField == self.passwordField ) {
-            textField.resignFirstResponder() // Hide the keyboard
-            attemptLogin() // Attempt to logon
+            self.passwordField.resignFirstResponder() // Hide the keyboard
+            attemptSignup() // Start the sign up process
         }
         // Anyways, return:
         return true
     }
     
-    func attemptLogin() {
-        // TODO: Start the login procedure
+    func attemptSignup() {
+        // TODO: Start the signup procedure
     }
-    
-    // MARK: - Helper functions here
-    
-    // REFERENCE: http://stackoverflow.com/a/29534779/705471
-    func resizeImage(image: UIImage, newHeight: CGFloat) -> UIImage {
-        let scale = newHeight / image.size.height
-        let newWidth = image.size.width * scale
-        UIGraphicsBeginImageContextWithOptions(CGSizeMake(newWidth, newHeight), false, 0)
-        image.drawInRect(CGRectMake(0, 0, newWidth, newHeight))
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        // Return now
-        return newImage
-    }
+
 }
